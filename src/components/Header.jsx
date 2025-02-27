52aa794a2c1d821e0adc902ea97bd89aa15cbba6
Rachel O <roconnellvisualartist@gmail.com>
2025-02-27 02:16:26 -0500

    responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,10 @@
 
 function Note () {
     return (
-        <main style={{width: '575px'}}>
-            <h2>Dear hooman, <br/> U have been slacking. I created Task Manager to make things simple for you. uze it. <br/> -serious 🐾</h2>
+        <main>
+            <div className="headNote">
+            <h2>Dear hooman, <br/> U have been slacking. I created Task Manager to make thingz simple for you. uze it. <br/> -serious 🐾</h2>
+            </div>
         </main>
     )
 }
